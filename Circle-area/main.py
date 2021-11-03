51fd06ba25bf7b61250936f25e04ed0827e45351
panda-sas <panda.sas20@gmail.com>
2021-11-03 11:54:04 -0500

    accepts the radius of a circle from the user and compute the area

---
+++ Circle-area/main.py
@@ -0,0 +1,7 @@
+# accepts the radius of a circle from the user and compute the area
+
+radius = float(input("Enter the radius: "))
+pi = 3.1416
+area = pi * radius ** 2
+
+print(f"Area is : {round(area, 2)}")
